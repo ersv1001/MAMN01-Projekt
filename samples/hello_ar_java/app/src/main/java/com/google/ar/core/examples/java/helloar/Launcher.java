06df757d74fd57592449f44540079c5644b2c454
Krom101 <ersv1001@gmail.com>
2020-04-28 11:30:13 +0200

    Startskärm med länk till kamera

---
+++ samples/hello_ar_java/app/src/main/java/com/google/ar/core/examples/java/helloar/Launcher.java
@@ -0,0 +1,20 @@
+package com.google.ar.core.examples.java.helloar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Launcher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+
+    public void openCamera(View view){
+        Intent intent = new Intent(this, HelloArActivity.class);
+        startActivity(intent);
+    }
+}
