e3231e00484b694180f784a759ec0376953ecf65
Oskar Wändesjö <oskar.wandesjo@gmail.com>
2020-05-11 14:55:18 +0200

    Implemented augmentedImage.

--- samples/hello_ar_java/app/src/main/java/com/google/ar/core/examples/java/helloar/ArCamera.java
+++ samples/hello_ar_java/app/src/main/java/com/google/ar/core/examples/java/helloar/ArCamera.java
@@ -9,94 +9,179 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 import com.google.ar.core.Anchor;
+import com.google.ar.core.AugmentedImage;
+import com.google.ar.core.Frame;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
+import com.google.ar.core.Session;
+import com.google.ar.core.TrackingState;
+import com.google.ar.core.examples.java.common.helpers.AugmentedImageNode;
+import com.google.ar.core.examples.java.common.helpers.SnackbarHelper;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.FrameTime;
 import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
+// TODO clean up code :)
 /**
  * This is an example activity that uses the Sceneform UX package to make common AR tasks easier.
  */
+//public class ArCamera extends AppCompatActivity {
+//    private static final String TAG = ArCamera.class.getSimpleName();
+//    private static final double MIN_OPENGL_VERSION = 3.0;
+//
+//    private ArFragment arFragment;
+//    private ViewRenderable pictureRenderable;
+//
+//    @Override
+//    @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
+//    // CompletableFuture requires api level 24
+//    // FutureReturnValueIgnored is not valid
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        if (!checkIsSupportedDeviceOrFinish(this)) {
+//            return;
+//        }
+//
+//        setContentView(R.layout.activity_arcamera);
+//        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+//
+//
+//        ViewRenderable.builder()
+//                .setView(this, R.layout.testfile)
+//                .build()
+//                .thenAccept(renderable -> pictureRenderable = renderable);
+//
+//        arFragment.setOnTapArPlaneListener(
+//                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
+//                    if (pictureRenderable == null) {
+//                        return;
+//                    }
+//
+//                    // Create the Anchor.
+//                    Anchor anchor = hitResult.createAnchor();
+//                    AnchorNode anchorNode = new AnchorNode(anchor);
+//                    anchorNode.setParent(arFragment.getArSceneView().getScene());
+//
+//                    // Create the transformable andy and add it to the anchor.
+//                    TransformableNode andy = new TransformableNode(arFragment.getTransformationSystem());
+//                    andy.setParent(anchorNode);
+//                    andy.setLocalRotation(Quaternion.axisAngle(new Vector3(-1f, 0, 0), 90f));
+//                    andy.setRenderable(pictureRenderable);
+//                    andy.select();
+//                });
+//    }
+//
+//    /**
+//     * Returns false and displays an error message if Sceneform can not run, true if Sceneform can run
+//     * on this device.
+//     *
+//     * <p>Sceneform requires Android N on the device as well as OpenGL 3.0 capabilities.
+//     *
+//     * <p>Finishes the activity if Sceneform can not run
+//     */
+//    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+//        if (Build.VERSION.SDK_INT < VERSION_CODES.N) {
+//            Log.e(TAG, "Sceneform requires Android N or later");
+//            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
+//            activity.finish();
+//            return false;
+//        }
+//        String openGlVersionString =
+//                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+//                        .getDeviceConfigurationInfo()
+//                        .getGlEsVersion();
+//        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+//            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+//            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+//                    .show();
+//            activity.finish();
+//            return false;
+//        }
+//        return true;
+//    }
+//}
+
 public class ArCamera extends AppCompatActivity {
-    private static final String TAG = ArCamera.class.getSimpleName();
-    private static final double MIN_OPENGL_VERSION = 3.0;
 
     private ArFragment arFragment;
-    private ViewRenderable pictureRenderable;
+    private ImageView fitToScanView;
+
+    // Augmented image and its associated center pose anchor, keyed by the augmented image in
+    // the database.
+    private final Map<AugmentedImage, AugmentedImageNode> augmentedImageMap = new HashMap<>();
 
     @Override
-    @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
-    // CompletableFuture requires api level 24
-    // FutureReturnValueIgnored is not valid
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (!checkIsSupportedDeviceOrFinish(this)) {
-            return;
-        }
-
         setContentView(R.layout.activity_arcamera);
+
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+        fitToScanView = findViewById(R.id.image_view_fit_to_scan);
 
+        arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdateFrame);
+    }
 
-        ViewRenderable.builder()
-                .setView(this, R.layout.testfile)
-                .build()
-                .thenAccept(renderable -> pictureRenderable = renderable);
-
-        arFragment.setOnTapArPlaneListener(
-                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
-                    if (pictureRenderable == null) {
-                        return;
-                    }
-
-                    // Create the Anchor.
-                    Anchor anchor = hitResult.createAnchor();
-                    AnchorNode anchorNode = new AnchorNode(anchor);
-                    anchorNode.setParent(arFragment.getArSceneView().getScene());
-
-                    // Create the transformable andy and add it to the anchor.
-                    TransformableNode andy = new TransformableNode(arFragment.getTransformationSystem());
-                    andy.setParent(anchorNode);
-                    andy.setLocalRotation(Quaternion.axisAngle(new Vector3(-1f, 0, 0), 90f));
-                    andy.setRenderable(pictureRenderable);
-                    andy.select();
-                });
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (augmentedImageMap.isEmpty()) {
+            fitToScanView.setVisibility(View.VISIBLE);
+        }
     }
 
     /**
-     * Returns false and displays an error message if Sceneform can not run, true if Sceneform can run
-     * on this device.
+     * Registered with the Sceneform Scene object, this method is called at the start of each frame.
      *
-     * <p>Sceneform requires Android N on the device as well as OpenGL 3.0 capabilities.
-     *
-     * <p>Finishes the activity if Sceneform can not run
+     * @param frameTime - time since last frame.
      */
-    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
-        if (Build.VERSION.SDK_INT < VERSION_CODES.N) {
-            Log.e(TAG, "Sceneform requires Android N or later");
-            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
-            activity.finish();
-            return false;
+    private void onUpdateFrame(FrameTime frameTime) {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+
+        // If there is no frame or ARCore is not tracking yet, just return.
+        if (frame == null || frame.getCamera().getTrackingState() != TrackingState.TRACKING) {
+            return;
         }
-        String openGlVersionString =
-                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
-                        .getDeviceConfigurationInfo()
-                        .getGlEsVersion();
-        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
-            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
-            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
-                    .show();
-            activity.finish();
-            return false;
+
+        Collection<AugmentedImage> updatedAugmentedImages = frame.getUpdatedTrackables(AugmentedImage.class);
+        for (AugmentedImage augmentedImage : updatedAugmentedImages) {
+            switch (augmentedImage.getTrackingState()) {
+                case PAUSED:
+                    // When an image is in PAUSED state, but the camera is not PAUSED, it has been detected,
+                    // but not yet tracked.
+                    String text = "Detected Image " + augmentedImage.getIndex();
+                    SnackbarHelper.getInstance().showMessage(this, text);
+                    break;
+
+                case TRACKING:
+                    // Have to switch to UI Thread to update View.
+                    fitToScanView.setVisibility(View.GONE);
+
+                    // Create a new anchor for newly found images.
+                    if (!augmentedImageMap.containsKey(augmentedImage)) {
+                        AugmentedImageNode node = new AugmentedImageNode(this);
+                        node.setImage(augmentedImage);
+                        augmentedImageMap.put(augmentedImage, node);
+                        arFragment.getArSceneView().getScene().addChild(node);
+                    }
+                    break;
+
+                case STOPPED:
+                    augmentedImageMap.remove(augmentedImage);
+                    break;
+            }
         }
-        return true;
     }
 }
